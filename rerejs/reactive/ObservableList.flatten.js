abcb883a3b6ab862e601a63fcfe8b1898701a0cf
Denis Rystsov <rystsov.denis@gmail.com>
2013-06-13 09:26:18 +0400

    finishes flatten

--- rerejs/reactive/ObservableList.flatten.js
+++ rerejs/reactive/ObservableList.flatten.js
@@ -3,7 +3,53 @@ define(
 function(ObservableListCore) {
 
 return function() {
-	throw new Error();
+    var result = new ObservableListCore([]);
+    var groups = {};
+    this.subscribe(handler({
+        data: function(items) {
+            for (var i in items) {
+                this.add(items[i]);
+            }
+        },
+        add: function(item) {
+            groups[item.key] = new Object();
+            var sublist = item.value, group = groups[item.key];
+            group.remap = {};
+            group.dispose = sublist.subscribe(handler({
+                data: function(items) {
+                    for (var i in items) {
+                        this.add(items[i]);
+                    }
+                },
+                add: function(item) {
+                    group.remap[item.key] = result.addValue(item.value);
+                },
+                remove: function(key) {
+                    result.remove(group.remap[key]);
+                    delete group.remap[key];
+                },
+            }));
+        },
+        remove: function(key) {
+            groups[key].dispose();
+            for (var old in groups[key].remap) {
+                result.remove(groups[key].remap[old]);
+            }
+        }
+    }));
+    return result;
+}
+
+function handler(handlers) {
+    return function(e) {
+        while(true) {
+            if (e[0]==="data") break;
+            if (e[0]==="add") break;
+            if (e[0]==="remove") break;
+            throw new Error();
+        }
+        handlers[e[0]].call(handlers, e[1]);
+    }
 }
 
 });
--- tests/ObservableList.flatten.js
+++ tests/ObservableList.flatten.js
@@ -5,8 +5,70 @@ var ObservableList = rere.reactive.ObservableList;
 var Variable = rere.reactive.Variable;
 var adt = rere.adt;
 
+exports.init = function(test) {
+    test.expect(1);
 
-exports.list = function(test){
+    var volume = new ObservableList([]);
+
+    var source1 = new ObservableList([1, 2]);
+    volume.addValue(source1);
+
+    var source2 = new ObservableList([3]);
+    volume.addValue(source2);
+
+    var target = volume.flatten();
+
+    checkObservableList(test, target, [1, 2, 3]);
+
+    test.done();
+};
+
+exports.add = function(test) {
+    test.expect(1);
+
+    var volume = new ObservableList([]);
+
+    var source1 = new ObservableList([]);
+    volume.addValue(source1);
+
+    var source2 = new ObservableList([]);
+    volume.addValue(source2);
+
+    var target = volume.flatten();
+
+    source1.addValue(1);
+    source1.addValue(2);
+    source2.addValue(3);
+
+    checkObservableList(test, target, [1, 2, 3]);
+
+    test.done();
+};
+
+exports.remove = function(test){
+    test.expect(1);
+
+    var volume = new ObservableList([]);
+
+    var source1 = new ObservableList([]);
+    volume.addValue(source1);
+
+    var source2 = new ObservableList([]);
+    volume.addValue(source2);
+
+    var target = volume.flatten();
+
+    var key11 = source1.addValue(1);
+    var key12 = source1.addValue(2);
+    var key21 = source2.addValue(3);
+    source1.remove(key12);
+
+    checkObservableList(test, target, [1, 3]);
+
+    test.done();
+};
+
+exports.removeBatch = function(test){
     test.expect(1);
 
     var volume = new ObservableList([]);
@@ -19,7 +81,14 @@ exports.list = function(test){
 
     var target = volume.flatten();
 
-    checkObservableList(test, target, []);
+    var key11 = source1.addValue(1);
+    var key12 = source1.addValue(2);
+    var key21 = source2.addValue(3);
+
+    volume.remove(key1);
+    source1.addValue(5);
+
+    checkObservableList(test, target, [3]);
     
     test.done();
 };
