076a76221ac9d0cfab617abf8f7fa29dc0bf0491
Denis Rystsov <rystsov.denis@gmail.com>
2013-06-13 09:40:37 +0400

    adds dict init

--- rerejs/adt/adt.js
+++ rerejs/adt/adt.js
@@ -2,6 +2,13 @@ define(["rere/adt/maybe", "rere/adt/error", "rere/adt/defaultdict"], function(ma
     return {
         "maybe" : maybe,
         "error" : error,
-        "Defaultdict": Defaultdict
+        "Defaultdict": Defaultdict,
+        "dict" : function(items){
+        	var dict = {};
+        	for (var i=0;i<items.length;i+=2) {
+        		dict[items[i]] = items[i+1];
+        	}
+        	return dict;
+        }
     }
 });
